faf0b90e64521f46d8a947d2a6af65c53536f0dd
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-06-09 06:20:50 +0700

    update userbot/plugins/pinterest.py

--- userbot/plugins/pinterest.py
+++ userbot/plugins/pinterest.py
@@ -7,12 +7,7 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest as unblock
-from telethon.tl.functions.messages import DeleteHistoryRequest
 
-
-from userbot import Convert, catub 
-from ..helpers.utils import get_user_from_event, reply_id
-from ..core.managers import edit_delete, edit_or_reply
 
 plugin_category = "fun"
 
@@ -33,22 +28,43 @@ from . import *
     pattern="sosmed(?:\s|$)([\s\S]*)",
     command=("sosmed", plugin_category),
     info={
-        "header": "To get view counter for the message. that is will delete old message and send new message where you can see how any people saw your message",
+        "header": "download TIKTOK, pinterest, Instagram",
         "usage": "{tr}sosmed",
     },
 )
-async def pntr(event):
-    if xxnx := event.pattern_match.group(1):
-        link = xxnx
-    elif event.is_reply:
-        link = await event.get_reply_message()
+async def _(event):
+    "download sosmed"
+    reply_to = await reply_id(event)
+    input_str = event.pattern_match.group(1)
+    reply = await event.get_reply_message()
+    message = ""
+    messages_id = []
+    if reply and input_str and input_str.isnumeric():
+        messages_id.append(reply.id)
+        async for message in event.client.iter_messages(
+            event.chat_id,
+            limit=(int(input_str) - 1),
+            offset_id=reply.id,
+            reverse=True,
+        ):
+            if message.id != event.id:
+                messages_id.append(message.id)
+    elif (
+        reply
+        and (not input_str or not input_str.isnumeric())
+        and input_str
+        or not reply
+        and input_str
+    ):
+        message = input_str
+    elif reply and (not input_str or not input_str.isnumeric()) and not input_str:
+        messages_id.append(reply.id)
     else:
-        return await edit_delete(event, "`Berikan link tautan pinterest...`")
-
-    chat = "@SaveAsBot"
-
-    catevent = await edit_or_reply(event, "`processing`")
-
+        return await edit_delete(
+            event, "`Either reply to message or give input to function properly`"
+        )
+    chat = "@SaveAsbot"
+    catevent = await edit_or_reply(event, "```processing```")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -64,9 +80,10 @@ async def pntr(event):
                 )
             response = await response
         except YouBlockedUserError:
-            return await catevent.edit("```Please unblock me (@saveasbot) u Nigga```")
+            return await catevent.edit("```Please unblock me (@SaveAsbot) u Nigga```")
         await event.client.send_read_acknowledge(conv.chat_id)
         await catevent.delete()
         await event.client.send_message(
             event.chat_id, response.message, reply_to=reply_to
         )
+            
