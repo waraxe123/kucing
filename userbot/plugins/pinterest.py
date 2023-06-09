3cc8839e987f37491ff42d354f94029c356bb296
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-06-09 09:12:24 +0700

    update userbot/plugins/pinterest.py

--- userbot/plugins/pinterest.py
+++ userbot/plugins/pinterest.py
@@ -88,35 +88,21 @@ async def _(event):
             response = await response
         except YouBlockedUserError:
             await event.client(UnblockRequest(chat))
-
             await event.client.send_message(chat, link)
-
             response = await response
-
         if response.text.startswith("Forward"):
-
             await catevent.edit("`Mengunggah...`")
-
         else:
-
             await catevent.delete()
-
             await event.client.send_file(
-
                 event.chat_id,
-
-                response.message.media,
-
-                
-
+                response.message.media
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             await catevent.delete()
-           
-
             event.chat_id, response.message, reply_to=reply_to
 
         )
             
-        )
+        .
             
